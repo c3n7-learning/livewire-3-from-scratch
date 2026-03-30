e611c3e133c4cc301c2426eb45334a8e4dac625c
Timothy Karani <kartimothy@gmail.com>
2026-03-30 23:23:00 +0300

    feat: using events

--- app/Livewire/Search.php
+++ app/Livewire/Search.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Article;
+use Livewire\Attributes\On;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 
@@ -28,6 +29,7 @@ class Search extends Component
             ->get();
     }
 
+    #[On('search:clear-results')]
     public function clear()
     {
         $this->reset('results', 'searchText');
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -905,7 +905,8 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+<body class="font-sans antialiased dark:bg-black dark:text-white/50" x-data
+    x-on:click="$dispatch('search:clear-results')">
     <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
         <div class="relative flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
@@ -930,6 +931,12 @@
             </div>
         </div>
     </div>
+
+    <script>
+        document.addEventListener('search:clear-results', function() {
+            console.log("Cleared results");
+        })
+    </script>
 </body>
 
 </html>
