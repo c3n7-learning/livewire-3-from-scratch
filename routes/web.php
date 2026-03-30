9ee63a25e1ebb3528fafd03aaa7ae30f81aeaf03
Timothy Karani <kartimothy@gmail.com>
2026-03-31 00:07:45 +0300

    feat: building admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,22 @@
 <?php
 
 use App\Livewire\ArticleIndex;
+use App\Livewire\ArticleList;
+use App\Livewire\Dashboard;
 use App\Livewire\ShowArticle;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', ArticleIndex::class);
-
 Route::get('/articles/{article}', ShowArticle::class);
+Route::get('/dashboard', Dashboard::class);
+Route::get('/dashboard/articles', ArticleList::class);
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
