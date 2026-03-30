d0ec3ac8d6d327418c3e6e27afff858f8430675c
Timothy Karani <kartimothy@gmail.com>
2026-03-31 00:33:17 +0300

    feat: using form objects

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Livewire\ArticleIndex;
 use App\Livewire\ArticleList;
+use App\Livewire\CreateArticle;
 use App\Livewire\Dashboard;
+use App\Livewire\EditArticle;
 use App\Livewire\ShowArticle;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +12,8 @@ Route::get('/', ArticleIndex::class);
 Route::get('/articles/{article}', ShowArticle::class);
 Route::get('/dashboard', Dashboard::class);
 Route::get('/dashboard/articles', ArticleList::class);
+Route::get('/dashboard/articles/create', CreateArticle::class);
+Route::get('/dashboard/articles/{article}/edit', EditArticle::class);
 
 // Route::middleware([
 //     'auth:sanctum',
