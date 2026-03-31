0255656810c73f76451e9a02836ae871624e49c3
Timothy Karani <kartimothy@gmail.com>
2026-04-01 01:47:56 +0300

    feat: offline states

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -2,7 +2,7 @@
     <form>
         <div class="flex gap-2">
             <input type="text" class="w-9/12 p-4 border rounded-md bg-gray-700 text-white" autocomplete="off"
-                wire:model.live.debounce='searchText' placeholder="{{ $placeholder }}" />
+                wire:model.live.debounce='searchText' placeholder="{{ $placeholder }}" wire:offline.attr='disabled' />
 
             <button class="text-white font-medium rounded-md p-4 bg-indigo-600 disabled:bg-gray-500"
                 wire:click.prevent='clear()' {{ empty($searchText) ? 'disabled' : '' }}>
